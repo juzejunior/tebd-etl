c07aad4a48dd04eb5460362814426f67c6e66977
Jr <jr>
2020-03-17 01:22:53 -0300

    make it better

--- api/data/database.js
+++ api/data/database.js
@@ -14,6 +14,7 @@ const getArtigo = async id => {
     const artigo = await conn.query(
       `SELECT a.numero_inscricao as id, a.nome as nome, p.nome as autor, ava.nota as nota, ava.comentario as comentario, p.telefone as contato from artigo as a INNER JOIN inscricao as i on a.FKInscricao = i.numero INNER JOIN participante as p on i.idParticipante = p.idParticipante INNER JOIN  avaliacao as ava on ava.Pkavaliacao = a.FKAvaliacao WHERE a.numero_inscricao = ${id}`
     );
+    conn.end();
     return artigo[0];
   } catch (err) {
     console.log(err);
